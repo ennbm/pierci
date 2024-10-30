c3566214482c45004b40fe945bb8b1c948a8b776
ennbm bc2024-3 <bozhenamrtnk@gmail.com>
2024-10-30 14:49:23 +0200

    Створено основний файл серверу + додано пакети, які будуть використовуватись в роботі

---
+++ server.js
@@ -0,0 +1,6 @@
+require ('dotenv').config;
+const express = require ('express');
+const mysql = require ('mysql2/promise');
+const jwt = require ('jsonwebtoken');
+const bcrtypt = require ('bcrtypt');
+const { configDotenv } = require('dotenv');
